c8fad0415c114f38d80fbc2517875163ee8d5dae
luisGeovany <glopezvan@gmail.com>
2020-10-21 19:22:37 -0500

    Formulario taller

--- formulario.php
+++ formulario.php
@@ -3,60 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tienda Web</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <link rel="stylesheet" href="css/estilos.css">
-    <link href="https://fonts.googleapis.com/css2?family=Courgette&display=swap" rel="stylesheet">
-    
+    <title>Formulario de Registro</title>
 </head>
 <body>
-    <header>
-        <h1>*** ADMINISTRACION DE PRODUCTOS ***</h1>
-        <hr>
-    </header>
-    <main>
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-6">
-                <form class="form" action="formulario.php" method="POST">
-                <div class="row">
-                        <div class="col-6">
-                            <label for="precio">Nombre Producto:</label>
-                            <input type="text" class="form-control" placeholder="Ingrese el Nombre" required>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-6">
-                            <label for="producto">Marca del producto:</label>
-                            <input type="text" class="form-control" placeholder=" Ingrese la Marca" required>
-                        </div>
-                        <div class="col-6">
-                            <label for="marca">Precio del Producto:</label>
-                            <input type="text" class="form-control" placeholder="Ingrese el Precio" required>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col">
-                            <label for="descripcion">Descripcion Producto:</label>
-                            <textarea class="form-control" id="descripcion" rows="3" placeholder="Ingrese una Descripcion"></textarea>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col">
-                            <button type="button" class="btn btn-success">REGISTRAR</button>
-                        </div>
-                    </div>
-                </form>
-            </div>
+    <h1>Administracion de Productos</h1>
+    <form class="form-register" action="registrar.php" method="post">
+        <h2 class="form-titulo">REGISTRA UN PRODUCTO</h2>
+        <div class="contenedor-inputs">
+            <input type="text" class="input-100" name="nombre" placeholder="Nombre del Producto" required>
+            <input type="text"class="input-50" name="marca"placeholder="Marca del Producto"required>
+            <input type="text"class="input-50" name="precio" placeholder="Precio del Producto"required>
+            <input type="submit" value="REGISTRAR" class="btn-enviar">
+            <p></p>
+
+
         </div>
-        </div>
-    </main>
-    <footer></footer>
+    </form>
     
-
-
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 </body>
 </html>
